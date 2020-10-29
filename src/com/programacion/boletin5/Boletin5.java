c99049c21df13e4443625b256caca881f804fc8b
José Ramón Aira Gil <jairagil@danielcastelao.org>
2020-10-29 13:51:34 +0100

    Arreglos

--- src/com/programacion/boletin5/Boletin5.java
+++ src/com/programacion/boletin5/Boletin5.java
@@ -7,8 +7,8 @@ public class Boletin5 {
 		consumo0.setPGas(1.57f);
 
 		Consumo consumo1 = new Consumo(333, 21, 1.50f, 97);
-		System.out.printf("Consumo medio: %.2f \n", consumo1.consumoMedio());
+		System.out.printf("Consumo medio: %.2f l\n", consumo1.consumoMedio());
 		consumo1.setLitros(22);
-		System.out.printf("Velocidad media: %.2f \n", consumo1.getVMed());
+		System.out.printf("Velocidad media: %.2f Km/h\n", consumo1.getVMed());
 	}
 }
