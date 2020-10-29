d9346237fae2bc1778c7dd96f61279e7c44e63ab
José Ramón Aira Gil <jairagil@danielcastelao.org>
2020-10-29 13:50:19 +0100

    Boletín 6 añadido

---
+++ src/com/programacion/boletin6/ejercicio3/Boletin6_3.java
@@ -0,0 +1,8 @@
+package com.programacion.boletin6.ejercicio3;
+
+public class Boletin6_3 {
+	public static void main(String[] args) {
+		Circulo circulo = new Circulo(1.5);
+		System.out.printf("El area del circulo es de %.2f y su perimetro de %.2f", circulo.calcularArea(), circulo.calcularLonxitude());
+	}
+}
